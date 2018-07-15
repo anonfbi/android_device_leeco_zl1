d58f64448b7acfd799972b685bb5bb9cd0a28ea7
anonfbi <freddyarenas2@gmail.com>
2018-07-15 03:43:13 +0000

    Tweak interactive Governor with 20S values

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -53,7 +53,6 @@ echo 19000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time
 echo 79000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis
 echo 307200 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/ignore_hispeed_on_notif
-echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/enable_prediction
 
 # Online the big cluster.
 echo 1 > /sys/devices/system/cpu/cpu2/online
@@ -69,10 +68,9 @@ echo 1248000 > /sys/devices/system/cpu/cpu2/cpufreq/interactive/hispeed_freq
 echo 1 > /sys/devices/system/cpu/cpu2/cpufreq/interactive/io_is_busy
 echo "85 1500000:90 1800000:70 2100000:95" > /sys/devices/system/cpu/cpu2/cpufreq/interactive/target_loads
 echo 19000 > /sys/devices/system/cpu/cpu2/cpufreq/interactive/min_sample_time
-echo 59000 > /sys/devices/system/cpu/cpu2/cpufreq/interactive/max_freq_hysteresis
+echo 79000 > /sys/devices/system/cpu/cpu2/cpufreq/interactive/max_freq_hysteresis
 echo 307200 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
 echo 1 > /sys/devices/system/cpu/cpu2/cpufreq/interactive/ignore_hispeed_on_notif
-echo 1 > /sys/devices/system/cpu/cpu2/cpufreq/interactive/enable_prediction
 
 # Re-enable thermal and BCL hotplug.
 echo 1 > /sys/module/msm_thermal/core_control/enabled
